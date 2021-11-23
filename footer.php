f6e7fe10e2d81b5bd1e3ecbf16de874eb6cca910
Updesh <updesh7457@gmail.com>
2021-11-24 01:27:10 +0530

    First Project Commit

---
+++ footer.php
@@ -0,0 +1,9 @@
+   
+    <div id="footer">
+        <div id="top" class="noprint"><p><span class="noscreen">Back on top</span> <a href="#header" title="Back on top ^">^<span></span></a></p></div>
+        <hr class="noscreen" />
+        
+        <p class="style1" id="createdby">created by Rovo's Team
+        </p>
+        <p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; my-path-alerts.com</p>
+</div> 
